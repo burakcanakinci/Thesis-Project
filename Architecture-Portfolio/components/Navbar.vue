204fbd750e770cc6add86be9583b1e9f6593ad37
Burak Can Akıncı <burakakinci.bca@gmail.com>
2022-06-01 16:56:42 +0300

    Navbar updated

---
+++ Architecture-Portfolio/components/Navbar.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="page-wrapper">
+        <div class="header w-nav" style="opacity: 1;">
+            <div class="container-default w-container">
+                <div class="header-wrapper">
+                    <div class="split-content header-right">
+                        <a href="/" class="brand w-nav-brand w--current">
+                            <img src="https://assets.website-files.com/61129bb2ecedf803564c68ec/612a715b08e8df7a8419c063_logo-you-x-webflow-template.svg" alt="Header Logo" class="header-logo">
+                        </a>
+                    </div>
+                    <div class="split-content header-center">
+                        <nav class="nav-menu w-nav-menu">
+                            <ul class="header-navigation">
+                                <li class="nav-item-wrapper">
+                                    <a href="/" class="nav-link w--current">Home</a>
+                                </li>
+                                <li class="nav-item-wrapper">
+                                    <a href="/" class="nav-link">About</a>
+                                </li>
+                                <li class="nav-item-wrapper">
+                                    <a href="/" class="nav-link">Portfolio</a>
+                                </li>
+                                <li class="nav-item-wrapper">
+                                    <a href="/" class="nav-link">Pages</a>
+                                </li>
+                            </ul>
+                            <a href="mailto:gulcatay.mimar@gmail.com" class="email-header-wrapper show-mobile w-inline-block">
+                                <img class="image email-header-icon" src="https://assets.website-files.com/61129bb2ecedf803564c68ec/6112a4fa0865622bd03376a4_email-icon-personal-x-template.svg" alt="email-header">
+                            <div>gulcatay.mimar@gmail.com</div>
+                            </a>
+                        </nav>
+                        <div class="menu-button w-nav-button">
+                            <div class="menu-button-icon-main-wrapper">
+                                <div class="menu-button-icon-wrapper">
+                                    <div class="menu-icon-line-top"></div>
+                                    <div class="menu-icon-line-middle"></div>
+                                    <div class="menu-icon-line-bottom"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="split-content header-left">
+                            <a href="mailto:gulcatay.mimar@gmail.com" class="email-header-wrapper w-inline-block">
+                                <img class="image email-header-icon" src="https://assets.website-files.com/61129bb2ecedf803564c68ec/6112a4fa0865622bd03376a4_email-icon-personal-x-template.svg" alt="Email Icon">
+                                <div>gulcatay.mimar@gmail.com</div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-nav-overlay"></div>
+            </div>
+        </div>
+    </div>
+</template>
