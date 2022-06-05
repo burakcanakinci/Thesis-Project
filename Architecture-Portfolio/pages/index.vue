e95f0fa8990ea07d111ffae0ffbbb1a28ac1c1de
Burak Can Akıncı <burakakinci.bca@gmail.com>
2022-06-05 20:16:45 +0300

    hero update

--- Architecture-Portfolio/pages/index.vue
+++ Architecture-Portfolio/pages/index.vue
@@ -1,14 +1,20 @@
 <template>
- 
-    <div class="page main-page">
-        <Navbar />
-   <Hero />
-    
+  <div class="page main-page">
+    <Navbar />
+    <Hero />
+    <MyServices />
+    <Projects />
+    <Aboutme />
   </div>
 </template>
 
 <script>
+import Hero from './Hero.vue';
+import MyServices from './MyServices.vue';
+import Projects from './Projects.vue';
+import Aboutme from './Aboutme.vue';
 export default {
-  name: 'IndexPage',
+    name: "IndexPage",
+    components: { Hero, MyServices, Projects, Aboutme}
 }
 </script>
