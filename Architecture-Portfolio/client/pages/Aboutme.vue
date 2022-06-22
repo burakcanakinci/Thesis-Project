01c4dbf369bd59f865b4ce5fbe8fc846655edbf6
Burak Can Akıncı <burakakinci.bca@gmail.com>
2022-06-22 21:22:31 +0300

    backend implementation

---
+++ Architecture-Portfolio/client/pages/Aboutme.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <div class="section wf-section">
+      <div class="container-default w-container" id="aboutMe">
+        <div class="home-about-wrapper">
+          <div
+            data-w-id="1b4fbc4a-e967-008a-2aa9-4904969e8761"
+            style="
+              transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1)
+                rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg);
+              opacity: 1;
+              transform-style: preserve-3d;
+            "
+            class="image-wrapper home-about"
+          >
+            <img
+              src="~/assets/image/pink-hat.jpeg"
+              sizes="(max-width: 479px) 93vw, (max-width: 767px) 94vw, (max-width: 991px) 95vw, (max-width: 1439px) 47vw, 589.4765625px"
+              alt="About Product Designer - You X Webflow Template"
+              class="image home-about image-smaller"
+            />
+          </div>
+          <div
+            data-w-id="21a4c434-3893-cfcf-0f70-4b8ab2950946"
+            style="
+              transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1)
+                rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg);
+              opacity: 1;
+              transform-style: preserve-3d;
+            "
+            class="split-content home-about"
+          >
+            <h2 class="title home-about">About me</h2>
+            <p class="paragraph home-about">
+              I love what I do and I love sharing my experience.<br>
+              You can see more about my background from below
+            </p>
+            <a>
+              <NuxtLink class="button-primary w-button" to="/AboutPage">More about me</NuxtLink>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div
+      data-w-id="e0fb2ffc-b1a3-478c-0a65-4f26db701dd7"
+      style="
+        transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg)
+          rotateY(0deg) rotateZ(0deg) skew(0deg);
+        transform-style: preserve-3d;
+        opacity: 1;
+      "
+      class="top-content resume-top-content see-full-resume"
+    >
+      <h2 class="title resume-section">Take a look at my resume</h2>
+      <img
+        src="https://assets.website-files.com/61129bb2ecedf803564c68ec/61140780d72abbf65771d050_resume-underline-personal-x-template.svg"
+        alt=""
+        class="image resume-underline"
+      /><a
+        href="Gulcatay-resume.pdf"
+        class="button-primary w-button"
+        download=""
+        >
+        See full resume
+        </a>
+    </div>
+  </div>
+</template>
+<script>
+
+</script>
+
+<!-- container default ustteki class icine -->
